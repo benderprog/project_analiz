df63044e6258992ee7069c8335c703b58809c0a4
benderprog <90317705+benderprog@users.noreply.github.com>
2026-02-09 12:12:03 +0300

    Make subdivision short_name migration safe

--- apps/portaldb/migrations/0002_subdivision_short_name.py
+++ apps/portaldb/migrations/0002_subdivision_short_name.py
@@ -7,9 +7,24 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="subdivision",
-            name="short_name",
-            field=models.CharField(blank=True, db_index=True, default="", max_length=255),
-        ),
+        migrations.SeparateDatabaseAndState(
+            state_operations=[
+                migrations.AddField(
+                    model_name="subdivision",
+                    name="short_name",
+                    field=models.CharField(
+                        blank=True,
+                        db_index=True,
+                        default="",
+                        max_length=255,
+                    ),
+                )
+            ],
+            database_operations=[
+                migrations.RunSQL(
+                    sql="ALTER TABLE subdivision ADD COLUMN IF NOT EXISTS short_name varchar(255);",
+                    reverse_sql=migrations.RunSQL.noop,
+                )
+            ],
+        )
     ]
