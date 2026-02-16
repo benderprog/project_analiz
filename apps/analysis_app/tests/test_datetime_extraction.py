fd6f67d1f448b138f8e692e2735a6cdc85768be3
benderprog <90317705+benderprog@users.noreply.github.com>
2026-02-16 11:50:23 +0300

    Fix datetime highlight helper NameError

--- apps/analysis_app/tests/test_datetime_extraction.py
+++ apps/analysis_app/tests/test_datetime_extraction.py
@@ -4,10 +4,18 @@ from datetime import datetime, timezone
 
 from django.test import SimpleTestCase
 
-from apps.analysis_app.services import _extract_datetime
+from apps.analysis_app.services import _extract_datetime, _find_datetime_span
 
 
 class DateTimeExtractionTest(SimpleTestCase):
+    def test_find_datetime_span_returns_match_for_word_date_and_time(self):
+        span = _find_datetime_span("24 марта 2022 года в 6:00 произошло событие")
+
+        self.assertIsNotNone(span)
+        if span is None:
+            self.fail("Expected datetime span for valid RU date/time text")
+        self.assertEqual("24 марта 2022 года", "24 марта 2022 года в 6:00 произошло событие"[span[0]:span[1]])
+
     def test_extracts_word_date_and_hhmm_time(self):
         dt, time_found = _extract_datetime("24 марта 2022 года в 6:00 произошло событие")
 
