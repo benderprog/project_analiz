f73ed8097527c4f5afeb28ad5e13ced33eb94d11
benderprog <90317705+benderprog@users.noreply.github.com>
2026-03-27 15:33:57 +0300

    Replace prod_ro SQL stubs with production schema queries

--- configs/portal/sql_prod_ro/event/event_offenders.sql
+++ configs/portal/sql_prod_ro/event/event_offenders.sql
@@ -5,6 +5,6 @@ select
   o.first_name,
   o.patronymic_name,
   o.date_of_birth
-from offenders o
+from resource.offenders o
 where o.event_id = any(%(event_ids)s)
 order by o.event_id, o.offender_id;
--- configs/portal/sql_prod_ro/event/event_snapshot.sql
+++ configs/portal/sql_prod_ro/event/event_snapshot.sql
@@ -4,5 +4,5 @@ select
   e.find_subdivision_unit_id as subdivision_id,
   e.event_type,
   e.article_of_law
-from event e
+from resource.event e
 where e.event_id = %(event_id)s;
